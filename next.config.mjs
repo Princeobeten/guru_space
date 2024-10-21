b01f01a9681d0ee240936f377537ba489a7bbb38
princeobeten <princeobeten56@gmail.com>
2024-10-21 23:35:27 +0100

    prop over loaded fixed

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: '/api/:path*', // Proxy to Backend
+        destination: '/api/:path*',
       },
     ];
   },

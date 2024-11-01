b2410f60cf616bfff8beb81e8c56b57b2afd0fb0
Prince Obeten <156962784+Princeobeten@users.noreply.github.com>
2024-11-01 09:56:20 +0100

    Update BookingSummaryPopup.tsx

--- app/(user dashboard)/dashboard/_components/BookingSummaryPopup.tsx
+++ app/(user dashboard)/dashboard/_components/BookingSummaryPopup.tsx
@@ -107,8 +107,8 @@ const BookingSummaryPopup: React.FC<BookingSummaryPopupProps> = ({
 
         {!paymentStatus && (
           <div className="mt-4 flex space-x-2">
-            {/* <Button className="w-full" onClick={handlePayWithCash}>Pay with Cash</Button> */}
-            <Button variant="outline" className="w-full" onClick={completeBooking}>Pay Now</Button>
+            <Button className="w-full" onClick={handlePayWithCash}>Pay with Cash</Button>
+{/*             <Button variant="outline" className="w-full" onClick={completeBooking}>Pay Now</Button> */}
           </div>
         )}
       </div>

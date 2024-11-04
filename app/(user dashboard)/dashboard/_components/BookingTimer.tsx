7b6065828f12ed8db97a643634990d9d7caee5a7
princeobeten <princeobeten56@gmail.com>
2024-11-04 19:03:40 +0100

    error fixed

--- app/(user dashboard)/dashboard/_components/BookingTimer.tsx
+++ app/(user dashboard)/dashboard/_components/BookingTimer.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React, { useState, useEffect } from 'react';
 import { updateDoc, doc, onSnapshot, Timestamp  } from 'firebase/firestore';
 import { db } from '@/lib/firebase';
